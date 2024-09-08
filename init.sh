2ddbf7133b4a9a95e0740098d7a095547b876ab7
CodingManoj <CodingManoj@gmail.com>
2024-09-08 02:29:52 -0400

    AutoPush

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 terraform init -backend-config=env-${ENV}/state.tfvars
-terraform ${ACTION} -auto-approve -var-file=env-${ENV}/main.tfvars -var vault-token=${VAULT_TOKEN} -var ssh_psw=${SSH_PSW}
+terraform ${ACTION} -auto-approve -var-file=env-${ENV}/main.tfvars -var vault-token=${VAULT_TOKEN} -var ssh_psw=${SSH_PASSWORD}
