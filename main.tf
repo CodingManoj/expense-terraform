7e3f5c4b74413bfb230cd7c76d3f23dcb2991bb7
CodingManoj <CodingManoj@gmail.com>
2024-09-07 23:08:50 -0400

    AutoPush

---
+++ main.tf
@@ -0,0 +1,33 @@
+module "frontend" {
+  depends_on = [module.backend]
+
+  source        = "git::https://github.com/B58-CloudDevOps/tf-module-terraform.git"
+  instance_type = var.components["frontend"]["instance_type"]
+  name          = var.components["frontend"]["name"]
+  env           = var.env
+  port_no       = var.components["frontend"]["port_no"]
+  pwd           = var.pwd
+
+}
+
+module "backend" {
+  depends_on = [module.mysql]
+
+  source        = "git::https://github.com/B58-CloudDevOps/tf-module-terraform.git"
+  instance_type = var.components["backend"]["instance_type"]
+  name          = var.components["backend"]["name"]
+  env           = var.env
+  port_no       = var.components["backend"]["port_no"]
+  pwd           = var.pwd
+
+}
+
+module "mysql" {
+  source        = "git::https://github.com/B58-CloudDevOps/tf-module-terraform.git"
+  instance_type = var.components["mysql"]["instance_type"]
+  name          = var.components["mysql"]["name"]
+  env           = var.env
+  port_no       = var.components["mysql"]["port_no"]
+  pwd           = var.pwd
+
+}
